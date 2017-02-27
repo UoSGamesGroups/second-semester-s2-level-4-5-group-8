34c25fab3f23cad0f91e63fc542233463b0edbf8
E-Ward <s182964@ucs.ac.uk>
2017-02-27 20:06:37 +0000

    Latest prototype

--- Prototypes/Catapult/Assets/Scripts/ArmRotate.cs
+++ Prototypes/Catapult/Assets/Scripts/ArmRotate.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class ArmRotate : MonoBehaviour {
 
+	public GameManager gameManager;
 	public int rotationOffset = 90;
-
+	public bool player1;
+	public bool player2;
 	
 	// Update is called once per frame
 	void Update () {
+		if (player1) {
+			if (gameManager.player1Turn) {
+				move ();
+			}
+		} 
+
+		if(player2){
+			if (gameManager.player2Turn) {
+				move ();
+			}
+		}
+	}
+
+	void move(){
 		Vector3 difference = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
 		difference.Normalize ();
 
--- Prototypes/Catapult/Assets/Scripts/MouseFire.cs
+++ Prototypes/Catapult/Assets/Scripts/MouseFire.cs
@@ -1,23 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MouseFire : MonoBehaviour {
 
-
+	public GameManager gameManager;
 	public GameObject bullet;
-	public float speed = 5.0f;
-
+	public float speed = 1.0f;
+	public float increase = 1.0f;
+	public bool player1;
+	public bool player2;
+	public Slider powerSlider;
 
 	// Use this for initialization
 	void Start () {
-
-	
+		powerSlider.maxValue = increase;
 	}
-	
+
+
+
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetMouseButtonDown(0)) {
+		if (player1) {
+			if (gameManager.player1Turn) {
+				Shoot ();
+				powerSlider.gameObject.SetActive (true);
+			} else {
+				powerSlider.gameObject.SetActive (false);
+			}
+		} 
+
+		if(player2){
+			if (gameManager.player2Turn) {
+				Shoot ();
+				powerSlider.gameObject.SetActive (true);
+			}else {
+				powerSlider.gameObject.SetActive (false);
+			}
+		}
+
+
+			
+	}
+
+	void Shoot(){
+		if (Input.GetMouseButton (0) && speed < 20) {
+			speed += increase*Time.deltaTime;
+			powerSlider.value = speed;
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
 			Vector2 target = Camera.main.ScreenToWorldPoint( new Vector2(Input.mousePosition.x,  Input.mousePosition.y) );
 			Vector2 myPos = new Vector2(transform.position.x,transform.position.y + 0);
 			Vector2 direction = target - myPos;
@@ -25,9 +58,9 @@ public class MouseFire : MonoBehaviour {
 			Quaternion rotation = Quaternion.Euler( 0, 0, Mathf.Atan2 ( direction.y, direction.x ) * Mathf.Rad2Deg );
 			GameObject projectile = (GameObject) Instantiate( bullet, myPos, rotation);
 			projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+			speed = 0; //Resets the force
+			powerSlider.value = speed;
 		}
-
-			
 	}
 
 
