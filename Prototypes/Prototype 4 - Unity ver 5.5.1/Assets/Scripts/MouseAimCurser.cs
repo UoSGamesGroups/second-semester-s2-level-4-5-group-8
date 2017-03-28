222d640d4a5144f1250b9993b7be74a8693f7ebe
E-Ward <s182964@ucs.ac.uk>
2017-03-28 22:16:12 +0100

    Latest version

---
+++ Prototypes/Prototype 4 - Unity ver 5.5.1/Assets/Scripts/MouseAimCurser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAimCurser : MonoBehaviour {
+
+	private Vector3 mousePosition;
+	public float movespeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		Cursor.visible = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			mousePosition = Input.mousePosition;
+			mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
+			transform.position = Vector2.Lerp(transform.position, mousePosition, movespeed);
+
+		
+	}
+}
+
